87e3aa46f4e794c099eccd9cb4f5319478a98412
Leo <workspace2707@yandex.ru>
2024-03-24 21:20:33 +0300

    created gsap animations for blocks

---
+++ src/assets/js/gsap.js
@@ -0,0 +1,244 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const tl = gsap.timeline();
+
+gsap.registerPlugin(ScrollTrigger);
+
+tl.fromTo(".header__wrapper", { opacity: 0 }, { opacity: 1, duration: 0.8 })
+  .fromTo(
+    ".header__nav",
+    {
+      y: "-30%",
+      opacity: 0,
+    },
+    {
+      y: "-5.5%",
+      opacity: 1,
+      duration: 0.8,
+    }
+  )
+  .fromTo(".header__cell", { scale: 0 }, { scale: 1, duration: 0.8 })
+  .fromTo(
+    ".header__multi",
+    { y: -100, opacity: 0 },
+    { y: 0, opacity: 1, duration: 0.8 }
+  )
+  .fromTo(
+    ".header__tech",
+    { y: 100, opacity: 0 },
+    { y: 0, opacity: 1, duration: 0.8 }
+  )
+  .fromTo(
+    ".header__person",
+    { translateY: "-200%", translateX: "-50%", opacity: 0 },
+    { translateY: "-50%", translateX: "-50%", opacity: 1, duration: 0.8 }
+  )
+  .fromTo(".header__particles", { opacity: 0 }, { opacity: 1, duration: 0.8 });
+
+gsap.fromTo(
+  ".about__title",
+  { x: -300 },
+  {
+    x: 0,
+    scrollTrigger: {
+      trigger: ".about",
+      scrub: true,
+      start: "top center",
+      end: "30% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".about__subtitle",
+  { x: -300 },
+  {
+    x: 0,
+    scrollTrigger: {
+      trigger: ".about",
+      scrub: true,
+      start: "top center",
+      end: "50% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".circle",
+  { rotate: 0 },
+  {
+    rotate: 360,
+    scrollTrigger: {
+      trigger: ".main",
+      scrub: true,
+      start: "top center",
+      end: "bottom center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".header__multi_development",
+  { x: "-30%" },
+  {
+    x: "0%",
+    scrollTrigger: {
+      trigger: ".development",
+      scrub: true,
+      start: "top center",
+      end: "center center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".header__tech_development",
+  { x: "30%" },
+  {
+    x: "0",
+    scrollTrigger: {
+      trigger: ".development",
+      scrub: true,
+      start: "top center",
+      end: "center center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".development__title",
+  { y: 100, opacity: 0 },
+  {
+    y: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".development",
+      scrub: true,
+      start: "top center",
+      end: "center center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".development__subtitle",
+  { y: 100, opacity: 0 },
+  {
+    y: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".development",
+      scrub: true,
+      start: "top center",
+      end: "center center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".developmet__images",
+  { y: 100, opacity: 0 },
+  {
+    y: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".development",
+      scrub: true,
+      start: "top center",
+      end: "center center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".marketing__title",
+  { x: -570 },
+  {
+    x: 0,
+    scrollTrigger: {
+      trigger: ".marketing",
+      scrub: true,
+      start: "top center",
+      end: "50% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".marketing__types",
+  { y: 100, opacity: 0 },
+  {
+    y: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".marketing",
+      scrub: true,
+      start: "top center",
+      end: "50% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".portfolio__title",
+  { y: 200, opacity: 0 },
+  {
+    y: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".portfolio",
+      scrub: true,
+      start: "top center",
+      end: "50% center",
+    },
+  }
+);
+
+const cardItems = document.querySelectorAll(".card__item");
+
+cardItems.forEach((cardItem) => {
+  gsap.fromTo(
+    cardItem,
+    { y: 100, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: cardItem,
+        scrub: true,
+        start: "start center",
+        end: "center center",
+      },
+    }
+  );
+});
+
+gsap.fromTo(
+  ".form_form",
+  { y: 100, opacity: 0 },
+  {
+    y: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".form",
+      scrub: true,
+      start: "top center",
+      end: "50% center",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".form__particles",
+  { opacity: 0 },
+  {
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".form",
+      scrub: true,
+      start: "top center",
+      end: "50% center",
+    },
+  }
+);
